0ecc64d7b5185123187067db922aa9b99fc591f7
unknown <222909071097-26>
2025-09-25 11:11:25 +0300

    добавлены

---
+++ 1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random(); // Создает генератор случайных чисел
+        int secretNumber = random.Next(1, 101); // Число от 1 до 100
+
+        Console.WriteLine($"Я загадал число от 1 до 100. Попробуй угадать!");
+        Console.ReadLine();
+        Console.WriteLine($"А нет, я его тебе сразу скажу: {secretNumber}!");
+        Console.ReadLine();
+    }
+}
